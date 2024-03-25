885c0ba1b5a200c0e848e9cc8544b07b829b87d5
ali-hbl <nwah.ally@gmail.com>
2024-03-25 17:57:07 +0100

    refactored informations

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -53,7 +53,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Ali.</span> I'm a
         <span className="font-bold"> front-end developer </span>
-        with <span className="font-bold">2 years</span> of experience. I enjoy building
+        with <span className="font-bold">3 years</span> of experience. I enjoy building
         <span className="italic"> sites & apps</span>. My focus is <span className="underline">React</span>.
       </motion.h1>
 
