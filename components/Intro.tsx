d2697d9f87a10e9634c14e90192fcde7d0ce6f12
ali-hbl <nwah.ally@gmail.com>
2024-10-08 17:27:31 +0200

    changed intro section

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -49,9 +49,8 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ali.</span> I'm a<span className="font-bold"> front-end developer </span>
-        with <span className="font-bold">3 years</span> of experience. I enjoy building
-        <span className="italic"> sites & apps</span>. My focus is <span className="underline">React</span>.
+        <span className="font-bold">Hello, I'm Ali.</span> I'm a<span className="font-bold"> Software Engineer</span>. I
+        enjoy building <span className="italic"> sites & apps</span>.
       </motion.h1>
 
       <motion.div
