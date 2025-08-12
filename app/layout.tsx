f3344093a95a9af41fe3ea420d3cc4884eda06e7
ali-hbl <nwah.ally@gmail.com>
2025-08-12 20:20:32 +0200

    fix: horizontal blur line on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,23 +20,38 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="!scroll-smooth">
-      <body
-        className={`${inter.className} relative bg-gray-50 pt-28 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 sm:pt-36`}
-      >
-        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
-        <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+<html lang="fr" className="!scroll-smooth">
+  <body
+    className={`${inter.className} relative bg-gray-50 pt-28 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 sm:pt-36`}
+  >
+    {/* Blob droit */}
+    <div
+      aria-hidden="true"
+      className="pointer-events-none absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full
+                 bg-[#fbe2e3] opacity-70 blur-none sm:opacity-100 sm:blur-[10rem] sm:w-[68.75rem]
+                 transform-gpu will-change-transform dark:bg-[#946263]"
+    ></div>
 
-        <ThemeContextProvider>
-          <ActiveSectionContextProvider>
-            <Header />
-            {children}
-            <Footer />
-            <Toaster position="top-right" />
-            <ThemeSwitch />
-          </ActiveSectionContextProvider>
-        </ThemeContextProvider>
-      </body>
-    </html>
+    {/* Blob gauche */}
+    <div
+      aria-hidden="true"
+      className="pointer-events-none absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full
+                 bg-[#dbd7fb] opacity-70 blur-none sm:opacity-100 sm:blur-[10rem] sm:w-[68.75rem]
+                 md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]
+                 transform-gpu will-change-transform dark:bg-[#676394]"
+    ></div>
+
+    <ThemeContextProvider>
+      <ActiveSectionContextProvider>
+        <Header />
+        {children}
+        <Footer />
+        <Toaster position="top-right" />
+        <ThemeSwitch />
+      </ActiveSectionContextProvider>
+    </ThemeContextProvider>
+  </body>
+</html>
+
   );
 }
