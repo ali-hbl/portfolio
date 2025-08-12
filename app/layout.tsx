3c85d534b56d4ebf7643758e7dfba7176bb6a1d6
ali-hbl <nwah.ally@gmail.com>
2025-08-12 20:34:46 +0200

    fix: option one reduce blur on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,13 +24,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body
         className={`${inter.className} relative min-h-screen overflow-x-hidden bg-gray-50 pt-28 text-gray-950 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90 sm:pt-36`}
       >
-        {/* wrapper background */}
         <div
           aria-hidden
           className="pointer-events-none absolute inset-0 -z-10 transform-gpu overflow-hidden will-change-transform [backface-visibility:hidden]"
         >
-          <div className="absolute right-[11rem] top-[-6rem] h-[31.25rem] w-[31.25rem] transform-gpu rounded-full bg-[#fbe2e3] blur-[10rem] will-change-transform [backface-visibility:hidden] dark:bg-[#946263] sm:w-[68.75rem]" />
-          <div className="absolute left-[-35rem] top-[-1rem] h-[31.25rem] w-[50rem] transform-gpu rounded-full bg-[#dbd7fb] blur-[10rem] will-change-transform [backface-visibility:hidden] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
+          <div className="absolute right-[11rem] top-[-6rem] h-[31.25rem] w-[31.25rem] transform-gpu rounded-full bg-[#fbe2e3] blur-[8rem] will-change-transform [backface-visibility:hidden] dark:bg-[#946263] sm:w-[68.75rem] sm:blur-[10rem]" />
+          <div className="absolute left-[-35rem] top-[-1rem] h-[31.25rem] w-[50rem] transform-gpu rounded-full bg-[#dbd7fb] blur-[8rem] will-change-transform [backface-visibility:hidden] dark:bg-[#676394] sm:w-[68.75rem] sm:blur-[10rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]" />
         </div>
 
         <ThemeContextProvider>
